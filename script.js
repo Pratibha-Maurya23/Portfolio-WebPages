d95cf802889458f337c0fe3a1e1d1bbe88464770
Pratibha1995 <pratibhacse20@gmail.com>
2024-12-06 23:33:34 +0530

    Add home Page

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Header Toggle
+
 let menuBtn = document.getElementById('MenuBtn')
 
-menuBtn.addEventListener('click', function(e) {
+menuBtn.addEventListener('click', function() {
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
     
 })
+
+
+
+// Typing Effect
+
+let typed = new Typed('.auto-input',{
+    strings: ['Front-End Developer!','Freelancer!','Coder!'],
+    typedSpeed: 1000,
+    backSpeed: 100,
+    backDelay: 200,
+    loop: true,
+}); 
