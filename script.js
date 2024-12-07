ca8715c79fa14fd7d1619894aeeff0f623c81cba
Pratibha1995 <pratibhacse20@gmail.com>
2024-12-07 14:10:51 +0530

    Add about section

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 // Header Toggle
 
-let menuBtn = document.getElementById('MenuBtn')
+let menuBtn = document.getElementById('MenuBtn');
 
-menuBtn.addEventListener('click', function() {
+menuBtn.addEventListener('click', function () {
     document.querySelector('body').classList.toggle('mobile-nav-active');
-    this.classList.toggle('fa-xmark')
-    
-})
-
+    this.classList.toggle('fa-xmark');
+});
 
 
 // Typing Effect
+console.log("hello");
 
 let typed = new Typed('.auto-input',{
     strings: ['Front-End Developer!','Freelancer!','Coder!'],
-    typedSpeed: 1000,
+    typeSpeed: 100,
     backSpeed: 100,
     backDelay: 200,
     loop: true,
